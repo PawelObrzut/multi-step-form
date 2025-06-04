130b9ec92b91beee8ffc069e8744df46715c1af3
PawelObrzut <pawel.r.obrzut@gmail.com>
2025-06-04 10:57:02 +0200

    resets css rules. completes steps buttons for mobile layout

---
+++ src/components/StepButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type Props = {
+  step: number
+  isActive: boolean
+  onClick: () => void
+}
+
+const StepButton = ({step, isActive, onClick}: Props) => {
+  return (
+    <button 
+      className={`stepButton ${isActive ? 'active' : ''}`}
+      onClick={onClick}
+    >
+      {step}
+    </button>
+  )
+}
+
+export default StepButton
