0d673ee3c262bb828f26ac39b937da9751d5d9ab
PawelObrzut <pawel.r.obrzut@gmail.com>
2025-06-05 08:55:54 +0200

    gets form input fields to notify validation status

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
+import { useForm } from "react-hook-form"
 import StepButton from './components/StepButton'
 import Step1 from './components/Step1';
 
+type Inputs = {
+  name: string
+  email: string
+  phoneNumber: string
+}
+
 function App() {
   const [activeStep, setActiveStep] = useState<number>(1);
-
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors } 
+  } = useForm<Inputs>();
+  
+  const submitForm = (data: Inputs) => {
+    console.log(data.name)
+    console.log(data.email)
+    console.log(data.phoneNumber)
+  }
   return (
     <>
       <nav className='navigationSteps '>
@@ -19,21 +36,25 @@ function App() {
           ))
         }
       </nav>
-        {
-          (() => {
+      <main className='personalInfo'>
+        <form onSubmit={handleSubmit(submitForm)}>
+          <section className='formInputFields'>
+          {(() => {
             switch (activeStep) {
               case 1:
-                return <Step1 />
-              // case 2:
-              //   return <Step2 />
-              default:
-                return null
-            }
-          })()
-        }
-      <section className='next-button-container'>
-        <button className='nextButton'>Next Step</button>
-      </section>
+                return <Step1 register={register} errors={errors} />
+                // case 2:
+                //   return <Step2 />
+                default:
+                  return null
+                }
+              })()}
+          </section>
+          <section className='next-button-container b'>
+            <button type='submit' className='nextButton'>Next Step</button>
+          </section>
+        </form>
+      </main>
     </>
   )
 }
--- src/components/Step1.tsx
+++ src/components/Step1.tsx
@@ -1,22 +1,62 @@
 import React from 'react'
+import type { FieldErrors, UseFormRegister } from "react-hook-form"
 
-const Step1 = () => {
+type Inputs = {
+  name: string
+  email: string
+  phoneNumber: string
+}
+
+type Props = {
+  register: UseFormRegister<Inputs>
+  errors: FieldErrors<Inputs>
+}
+
+const Step1 = ({ register, errors }: Props) => {
   return (
-     <main className='personalInfo'>
-        <h1>Personal Info</h1>
-        <p>Please provide your name, email address, and phone number.</p>
+    <>
+      <h1>Personal Info</h1>
+      <p>Please provide your name, email address, and phone number.</p>
 
-        <form>
-          <label htmlFor=''>Name</label>
-          <input type='text' placeholder='e.g. Stephen King' />
+      <div>
+        <label htmlFor='name'>Name</label>
+        {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+      </div>
+      <input 
+        id='name'
+        placeholder='e.g. Stephen King' 
+        className={errors.name ? 'inputError' : ''}
+        {...register('name', { required: 'This field is required' })} 
+      />
 
-          <label htmlFor="">Email Address</label>
-          <input type='text' placeholder='e.g. stephenking@lorem.com' />
+      <div>
+        <label htmlFor='emailAddress'>Email Address</label>
+        {errors.email && <span className='errorMessage'>{errors.email.message}</span>}
+      </div>
+      <input
+        id='emailAddress'
+        type="email"
+        placeholder='e.g. stephenking@lorem.com'
+        className={errors.email ? 'inputError' : ''}
+        {...register('email', { 
+          required: { value: true, message: 'This field is required' },
+          pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'Enter a valid email address'}
+        })}
+      />
 
-          <label htmlFor="">Phone Number</label>
-          <input type='text' placeholder='e.g. +1 234 567 890' />
-        </form>
-      </main>
+      <div>
+        <label htmlFor='phoneNumber'>Phone Number</label>
+        {errors.phoneNumber && <span className='errorMessage'>{errors.phoneNumber.message}</span>}
+      </div>
+      <input 
+        id='phoneNumber'
+        type='text'
+        className={errors.phoneNumber ? 'inputError' : ''}
+        placeholder='e.g. +1 234 567 890' 
+        {...register('phoneNumber', { required: 'This field is required' })}
+        />
+
+    </>
   )
 }
 
